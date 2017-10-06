efc645d74f888e4b5da2064dfcb08ef0ed99f5f9
Asep Muhidin <asep.muhidin@gmail.com>
2017-10-06 17:20:52 +0700

    Create operator.php

---
+++ operator.php
@@ -0,0 +1,20 @@
+<?php
+  
+$a=10;$b=20;
+
+$c=$b % $a;
+$a +=$c;
+$b *=$a;
+
+$c &=1;
+$c |=$a;
+
+$d=($a>$b);
+
+echo "Nilai a=$a <br>";
+echo "Nilai b=$b <br>";
+echo "Nilai c=$c <br>";
+echo "Nilai d=$d <br>";
+
+
+?>
