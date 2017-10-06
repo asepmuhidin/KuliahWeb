57870a8ba8300035404b7e4ce8a72b2d1b2e0441
Asep Muhidin <asep.muhidin@gmail.com>
2017-10-06 17:11:43 +0700

    Create hello_world.php

---
+++ hello_world.php
@@ -0,0 +1,13 @@
+<?php
+  echo "Hello world..";
+  echo "<br>";
+  
+  $str="Hello from variable..";
+  
+  echo $str;
+  echo "<br>";
+  
+  echo "Say..$str";
+  
+  echo "Hello world..<br> Say..$str"; 
+?>
